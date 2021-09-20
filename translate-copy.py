f4af539cc544b10da6df560aee13e38b8b53b24a
nmyraz <55395127+nmyraz@users.noreply.github.com>
2021-09-21 02:01:12 +0800

    Added translated data

--- translate-copy.py
+++ translate-copy.py
@@ -8,7 +8,7 @@ from googletrans import Translator
 if __name__ == "__main__":
 
     #set filename for input and output
-    fileName = "sops-cleaned"
+    fileName = "azmin-ali-vs-anwar-ibrahim-cleaned"
     input = f"{fileName}.csv"
     output = f"{fileName}-translated.csv"
 
